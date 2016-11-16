227aff07ca511234266494928c2a1701cb5c22d2
SISP\tomas <tomasj@inbox.lt>
2016-11-16 08:51:27 +0200

    2 paskaitos ND

---
+++ paskaita2Nd/src/lt/vcs/Paskaita2Nd.java
@@ -0,0 +1,17 @@
+package lt.vcs;
+
+/**
+ *
+ * @author tomas
+ */
+public class Paskaita2Nd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Padariau namu darbus");
+        // TODO code application logic here
+    }
+    
+}
