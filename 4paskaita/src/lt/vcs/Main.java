aa1a97b3a30b1ffca5c3fe6993441f64a3022bb4
SISP\tomas <tomasj@inbox.lt>
2016-11-17 18:48:41 +0200

    4 paskaita

---
+++ 4paskaita/src/lt/vcs/Main.java
@@ -0,0 +1,16 @@
+package lt.vcs;
+
+/**
+ *
+ * @author tomas
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ 4paskaita/src/lt/vcs/VcsUtils.java
@@ -0,0 +1,38 @@
+package lt.vcs;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tomas
+ */
+public class VcsUtils {
+    public static int inputInt(String text){
+    return (Integer) input(1, text);
+    }
+    public static String inputWord(String text){
+    return (String) input(2, text);
+    }
+    public static String inputLine(String text){
+    return (String) input(3, text);
+    }
+    private static Object input(int choice, String text){
+    println(text);
+    Object result = null;
+    if(choice == 1)
+        result = newScan().nextInt();
+    else if (choice == 2)
+        result = newScan().next();
+    else if (choice == 3)
+        result = newScan().nextLine();
+    return result;
+    }
+    
+    
+    public static Scanner newScan(){
+        return new Scanner(System.in);
+    } 
+    public static void println(String txt){
+      System.out.println(txt);  
+    }
+}
